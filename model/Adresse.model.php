7d8600b37e542a96ddd04a22da40fd8a175aa5ad
GuillaumeC <guillaumecarayon.64@gmail.com>
2014-03-10 10:08:31 +0100

    Modèle pour la gestion des données de l'adresse

---
+++ model/Adresse.model.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Modèle Adresse
+ *
+ * @author Guillaume
+ */
+include $models["Modele"];
+
+class Adresse extends Modele {
+
+    /**
+     * Insertion d'adresse
+     * 
+     * Permet d'insérer les informations concernant l'adresse
+     * Préféré une implémentation dans les méthodes d'insertions de type de compte particulier
+     * @param type $infosLoc
+     */
+    public function insertAdresse($infosLoc) {
+        $reqAdresse = "INSERT INTO ADRESSE VALUES(:numVoie, :nomVoie, :cptVoie, :cpAdresse, :villeAdresse, :dptAdresse, :regionAdresse, :paysAdresse";
+        $bdd = $this->getBdd();
+        $insertAdresse = $bdd->prepare($reqAdresse);
+        $insertAdresse->execute(array(
+            "numVoie" => $infosLoc["numVoie"],
+            "nomVoie" => $infosLoc["nomVoie"],
+            "cptVoie" => $infosLoc["cptVoie"],
+            "cpAdresse" => $infosLoc["cpAdresse"],
+            "villeAdresse" => $infosLoc["villeAdresse"],
+            "dptAdresse" => $infosLoc["dptAdresse"],
+            "regionAdresse" => $infosLoc["regionAdresse"],
+            "paysAdresse" => $infosLoc["paysAdresse"]));
+    }
+
+}
