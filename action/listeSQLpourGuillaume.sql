b2e4b7a807d4a31e17876e3e9426cdd76a9bf5c9
Gabriel Bellakhdar Piechaud <gabriel.bellakhdar_piechaud@ensc.fr>
2014-03-20 17:40:40 +0100

    Merge origin/master
    Conflicts:
    	bdd.sql
    	model/Adresse.model.php
    	model/Image.model.php
    	model/Organisateur.model.php
    	model/Participant.model.php

---
+++ action/listeSQLpourGuillaume.sql
@@ -0,0 +1,39 @@
+INSERT INTO sport VALUES ('','Football canadien');
+INSERT INTO sport VALUES ('','Nage avec palmes');
+INSERT INTO sport VALUES ('','Equitation Irlandaise');
+INSERT INTO sport VALUES ('','Alpinisme');
+INSERT INTO sport VALUES ('','Randonnée');
+INSERT INTO sport VALUES ('','Bascket');
+INSERT INTO sport VALUES ('','Tennis');
+INSERT INTO sport VALUES ('','Escrime');
+INSERT INTO sport VALUES ('','Natation');
+INSERT INTO sport VALUES ('','Surf');
+INSERT INTO sport VALUES ('','Hockey');
+INSERT INTO sport VALUES ('','Crosse');
+INSERT INTO sport VALUES ('','Lutte mongole');
+INSERT INTO sport VALUES ('','Lutte sénégalaise');
+INSERT INTO sport VALUES ('','Lutte bretonne');
+INSERT INTO sport VALUES ('','Lutte mongole');
+INSERT INTO sport VALUES ('','Lutte turque');
+INSERT INTO sport VALUES ('','Lutte birmane');
+INSERT INTO sport VALUES ('','Lutte greco-romaine');
+INSERT INTO sport VALUES ('','Lutte libre');
+INSERT INTO sport VALUES ('','Jeu de paume');
+INSERT INTO sport VALUES ('','Squash');
+INSERT INTO sport VALUES ('','vol à voile');
+INSERT INTO sport VALUES ('','parapente');
+INSERT INTO sport VALUES ('','Marche nordique');
+INSERT INTO sport VALUES ('','Bowling');
+INSERT INTO sport VALUES ('','Curve fever');
+INSERT INTO sport VALUES ('','Volley');
+INSERT INTO sport VALUES ('','Rugby à XV');
+
+
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
