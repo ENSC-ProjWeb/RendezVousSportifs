e4a3e0d5bc88089095cda3eeeb8701890d87269c
GuillaumeC <guillaumecarayon.64@gmail.com>
2014-03-25 18:07:27 +0100

    Ajout de la recherche d'événement et découpages de la récupération d'infos d'événement en plusieurs méthodes

--- model/Evenement.model.php
+++ model/Evenement.model.php
@@ -3,13 +3,16 @@
 class Evenement extends Modele
 {
     /**
+     * InsertEvenement
      * 
-     * @param type $infosEvenement
-     * @param type $infosLoc
-     * @param type $idOrganisateur
-     * @return type
+     * Permet d'insérer un événement dans la base de données
      * 
-     * @todo FAIRE LE LIEN AVEC LE SPORT !
+     * @param array $infosEvenement contient les informations de l'événement
+     * @param array $infosLoc contient les informations de localisation
+     * @param int $idOrganisateur contient l'id de l'organisateur
+     * @return un objet PDO
+     * 
+     * @todo Factoriser le code...
      */
     public function insertEvenement($infosEvenement, $infosLoc, $idOrganisateur)
     {
@@ -17,20 +20,20 @@ class Evenement extends Modele
         $adresse = new Adresse();
         $adresse->insertAdresse($infosLoc);
         $lastIdAdresse = $this->getLastId("idAdresse", "ADRESSE");
-        
+        $debutEvent = $infosEvenement['debutEvent'];
+        $finEvent = $infosEvenement['finEvent'];
         // On insère ensuite les informations concernant les événements
-        $evenement = "INSERT INTO EVENEMENT VALUES ('', :nomEvent, :nbParticipantsMax, :nbParticipantsMin, :prixEvent, :description, STR_TO_DATE(:debutEvent, '%d/%m/%Y %k:%i' ), STR_TO_DATE(:finEvent, '%d/%m/%Y %k:%i'), :idAdresse)";
+        $evenement = "INSERT INTO EVENEMENT VALUES ('', :nomEvent, :nbParticipantsMax, :nbParticipantsMin, :prixEvent, :description, :debutEvent, :finEvent, :idAdresse)";
         $paramsEvenement = array(
             "nomEvent" => $infosEvenement['nomEvenement'],
             "nbParticipantsMax" =>  $infosEvenement['nbMaxParticipants'],
             "nbParticipantsMin" => $infosEvenement['nbMinParticipants'],
             "prixEvent" => $infosEvenement['tarifEvenement'],
             "description" => $infosEvenement['descEvenement'],
-            "debutEvent" => $infosEvenement['debutEvent'],
-            "finEvent" => $infosEvenement['finEvent'],
+            "debutEvent" => "$debutEvent",
+            "finEvent" => "$finEvent",
             "idAdresse" => $lastIdAdresse
             );
-        
         $this->executerRequete($evenement, $paramsEvenement);
         $lastIdEvenement = $this->getLastIdEvent();
         
@@ -53,40 +56,79 @@ class Evenement extends Modele
         return $insertLinkEvtOrg;
     }
     
+    /**
+     * GetLastIdEvent
+     * 
+     * Permet de récupérer le dernier ID de l'événement inséré dans la table
+     * @return int
+     */
     public function getLastIdEvent() {
         return $this->getLastId("idEvent", "EVENEMENT");
     }
     
-    public function getInfosEventVignette($idEvent) {
-        // On récupère les images
+    /**
+     * GetFirstImageEvent
+     * 
+     * Permet de récupérer le premier image d'un événement (image insérée lors de l'inscription)
+     * @param type $idEvent
+     * @return type
+     */
+    public function getFirstImageEvent($idEvent) {
         $reqImageEvent = "SELECT nomImage, cibleImage FROM IMAGE, CONTENIR WHERE CONTENIR.idEvent = :idEvent AND CONTENIR.idImage = IMAGE.idImage";
         $paramsReqImageEvent = array("idEvent" => $idEvent);
         $resImageEvent = $this->executerRequete($reqImageEvent, $paramsReqImageEvent);
-        $infosImage  = $resImageEvent->fetch();
-        
-        // On récupère les informations  de l'événement
-        $reqEvent = "SELECT nomEvent, debutEvent FROM EVENEMENT WHERE idEvent = :idEvent ";
+        return $infosImage  = $resImageEvent->fetch();
+    }
+    
+    public function getAllImageEvent($idEvent) {
+        $reqImageEvent = "SELECT nomImage, cibleImage FROM IMAGE, CONTENIR WHERE CONTENIR.idEvent = :idEvent AND CONTENIR.idImage = IMAGE.idImage";
+        $paramsReqImageEvent = array("idEvent" => $idEvent);
+        $resImageEvent = $this->executerRequete($reqImageEvent, $paramsReqImageEvent);
+        return $infosImage  = $resImageEvent->fetchAll();
+    }
+    
+    
+    public function getPrimaryInfosEvent($idEvent) {
+        $reqEvent = "SELECT nomEvent, nbParticipantsMax, nbParticipantsMin, prixEvent, descriptionEvent, debutEvent, finEvent FROM EVENEMENT WHERE idEvent = :idEvent ";
         $paramsReqEvent = array("idEvent" => $idEvent);
         $resEvent = $this->executerRequete($reqEvent, $paramsReqEvent);
-        $infosEvenement = $resEvent->fetch();
-        
-        // On récupère les sports concernés
+        return $infosEvenement = $resEvent->fetch();
+    }
+    
+    
+    public function getLinkedSports($idEvent) {
         $reqSports = "SELECT nomSport FROM SPORT, REGROUPER WHERE REGROUPER.idEvent = :idEvent AND REGROUPER.idSport = SPORT.idSport";
         $paramReqSports = array("idEvent" => $idEvent);
         $resSports = $this->executerRequete($reqSports, $paramReqSports);
-        $infosSports = $resSports->fetchAll(PDO::FETCH_COLUMN);
-        
-        // On récupère la ville concernée
-        $reqVille = "SELECT villeAdresse FROM ADRESSE, EVENEMENT WHERE EVENEMENT.idEvent = :idEvent AND EVENEMENT.idAdresse = ADRESSE.idAdresse";
+        return $infosSports = $resSports->fetchAll(PDO::FETCH_COLUMN);
+    }
+    
+    public function getLinkedLocation($idEvent) {
+        $reqVille = "SELECT numVoieAdresse, nomVoieAdresse, cptAdresse, codePostalAdresse, villeAdresse, dptAdresse, regionAdresse, paysAdresse FROM ADRESSE, EVENEMENT WHERE EVENEMENT.idEvent = :idEvent AND EVENEMENT.idAdresse = ADRESSE.idAdresse";
         $paramReqVille = array("idEvent" => $idEvent);
         $resVille = $this->executerRequete($reqVille, $paramReqVille);
-        $infoVille = $resVille->fetch();
-        
-        // On récupère l'organisateur
-        $reqOrganisateur = "SELECT nomOrganisateur FROM ORGANISATEUR, ORGANISER WHERE ORGANISER.idEvent = :idEvent AND ORGANISER.idOrganisateur = ORGANISATEUR.idOrganisateur";
+        return $infoVille = $resVille->fetch();
+    }
+    
+    public function getOrganisateur($idEvent) {
+        $reqOrganisateur = "SELECT nomOrganisateur, typeOrganisateur, nomRef, prenomRef FROM ORGANISATEUR, ORGANISER WHERE ORGANISER.idEvent = :idEvent AND ORGANISER.idOrganisateur = ORGANISATEUR.idOrganisateur";
         $paramReqOrganisateur = array("idEvent" => $idEvent);
         $resOrganisateur = $this->executerRequete($reqOrganisateur, $paramReqOrganisateur);
-        $infoOrganisateur = $resOrganisateur->fetch(); 
+        return $infoOrganisateur = $resOrganisateur->fetch(); 
+    }
+    
+    
+    public function getInfosEventVignette($idEvent) {
+        // On récupère la première image
+        $infosImage = $this->getFirstImageEvent($idEvent);
+        // On récupère les informations  de l'événement
+        $infosEvenement = $this->getPrimaryInfosEvent($idEvent);
+        // On récupère les sports concernés
+        $infosSports = $this->getLinkedSports($idEvent);
+        // On récupère la ville concernée
+        $infoVille = $this->getLinkedLocation($idEvent);
+        // On récupère l'organisateur
+        $infoOrganisateur = $this->getOrganisateur($idEvent);
         
         return array("idEvent" => $idEvent,
                      "nomImage" => $infosImage["nomImage"],
@@ -95,10 +137,10 @@ class Evenement extends Modele
                      "nomEvenement" => $infosEvenement["nomEvent"],
                      "debutEvenement" => $infosEvenement["debutEvent"],
                      "sportsAssocies" => $infosSports,
-                     "ville" => $infoVille["villeAdresse"]);
-       
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+                     "ville" => $infoVille["villeAdresse"]);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
     }
+    
+    
     
     public function getListEvent() {
         $reqListEvent = "SELECT idEvent FROM EVENEMENT";
@@ -106,8 +148,72 @@ class Evenement extends Modele
         return $res->fetchAll(PDO::FETCH_COLUMN, 0);
     }
     
+    
+    
     public function getInfosEvent($idEvent) {
-        // On récupère les informations sur l'événement
-        $infosEvent = "SELECT nomEvent, nbParticipantsMax, nbParticipantsMin, prixEvent, descEvent, debutEvent, finEvent FROM EVENEMENT WHERE idEvenement = :idEvenement "
+        $infosEvent = $this->getPrimaryInfosEvent($idEvent);
+        $infosImage = $this->getAllImageEvent($idEvent);
+        $infosSport = $this->getLinkedSports($idEvent);
+        $infosLocation = $this->getLinkedLocation($idEvent);
+        $infosOrganisateur = $this->getOrganisateur($idEvent);
+        
+        return array("infosEvent" => $infosEvent,
+                     "imagesEvent" => $infosImage,
+                     "sportsAssocies" => $infosSport,
+                     "localisation" => $infosLocation,
+                     "infosOrganisateur" =>  $infosOrganisateur);
     }
+    
+    public function preInscrireMembre($idEvent, $login) {
+        // Je récupère l'ID du participant
+        $participant = new Participant();
+        $resParticipant = $participant->getParticipant($login);
+        $idParticipant = $resParticipant["idParticipant"];
+        
+        // J'insère dans la table inscrire 
+        $reqPreInscrire = "INSERT INTO INSCRIRE VALUES (:idParticipant, :idEvent, 'PI')";
+        $paramsReqPreInscrire = array("idParticipant" => $idParticipant, "idEvent" => $idEvent);
+        if ($this->executerRequete($reqPreInscrire, $paramsReqPreInscrire)) { return true; } else { return false; }
+    }
+    
+    public function validerInscriptionMembre($idEvent, $login) {
+        // Je récupère l'ID du participant
+        $participant = new Participant();
+        $resParticipant = $participant->getParticipant($login);
+        $idParticipant = $resParticipant["idParticipant"];
+        
+        // Je modifie la table inscrire
+        $reqValiderInscriptionMembre = "UPDATE INSCRIRE SET statutInscription = 'I' WHERE idParticipant = :idParticipant AND idEvent = :idEvent";
+        if ($this->executerRequete($reqValiderInscriptionMembre, array("idParticipant" => $idParticipant, "idEvent" => $idEvent))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    
+    public function refuserInscriptionMembre($idEvent, $login) {
+        // Je récupère l'ID du participant
+        $participant = new Participant();
+        $resParticipant = $participant->getParticipant($login);
+        $idParticipant = $resParticipant["idParticipant"];
+        
+        // Je modifie la table inscrire
+        $reqValiderInscriptionMembre = "UPDATE INSCRIRE SET statutInscription = 'R' WHERE idParticipant = :idParticipant AND idEvent = :idEvent";
+        if ($this->executerRequete($reqValiderInscriptionMembre, array("idParticipant" => $idParticipant, "idEvent" => $idEvent))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    
+    
+    public function getListEventSearched($motCle) {
+        $req = "SELECT idEvent FROM EVENEMENT WHERE nomEvent LIKE %:motCle%";
+        $res = $this->executerRequete($req, array("motCle" => $motCle));
+        $resultatRecherche = $res->fetchAll();
+        return $resultatRecherche;
+    }
+        
 }
